d35c22aa8a9e11f0e629344e5269d00d1c0c2f28
davimc <davimatosc@hotmail.com>
2022-11-15 10:06:56 -0300

    Project created

---
+++ backend/src/main/java/br/com/devsuperior/DSMeta/DsMetaApplication.java
@@ -0,0 +1,13 @@
+package br.com.devsuperior.DSMeta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsMetaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsMetaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/com/devsuperior/DSMeta/DsMetaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.devsuperior.DSMeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsMetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
